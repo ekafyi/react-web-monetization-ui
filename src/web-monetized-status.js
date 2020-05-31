1c3625c731bb86b5872dcf07d3b8b2aedcc694d1
Eka <6597211+ekafyi@users.noreply.github.com>
2020-05-31 21:35:35 +0700

    update css classname, remove active/inactive css, add data-web-monetization-status in Status component

--- src/web-monetized-status.js
+++ src/web-monetized-status.js
@@ -6,9 +6,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useMonetizationState } from 'react-web-monetization';
 
-const cssClass = 'rwmui-ms';
-const cssClassActive = 'rwmui-ms--monetized';
-const cssClassInactive = 'rwmui-ms--not-monetized';
+const cssClass = 'rwmui-status';
 
 const WebMonetizedStatus = ({
   loading,
@@ -19,16 +17,13 @@ const WebMonetizedStatus = ({
 }) => {
   const monetization = useMonetizationState();
 
-  const cssClassMerged = `${cssClass} ${className || ''} ${
-    monetization.state === 'started' ? cssClassActive : ''
-  } ${
-    monetization.state === 'stopped' || !monetization.state
-      ? cssClassInactive
-      : ''
-  }`;
-
   return (
-    <div aria-live='polite' className={cssClassMerged} {...props}>
+    <div
+      aria-live='polite'
+      className={`${cssClass} ${className || ''}`}
+      data-web-monetization-status={monetization.state === 'started' ? 1 : 0}
+      {...props}
+    >
       {monetization.state === 'pending' && loading}
       {monetization.state === 'started' && active}
       {(monetization.state === 'stopped' || !monetization.state) && inactive}
